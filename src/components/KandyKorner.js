4c671e042251ceb18e068d2f83dd4ffc056113d8
Kristen Carroll <carroll.kristen@gmail.com>
2022-03-01 13:05:44 -0600

    first commit inializing kandykorner

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,14 @@
+
+
+import React from "react"
+
+//(in react components, the HTML we write is called JSX)
+export const KandyKorner = () => {
+
+    //render CustomerList under h1
+      return (
+          <h1>Kandy Korner</h1>
+          
+          )
+
+}
