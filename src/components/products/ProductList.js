f83e4f3134f8dc012f53f71b1dac2eb727c5bd28
Kristen Carroll <carroll.kristen@gmail.com>
2022-03-02 10:36:29 -0600

    added sort query string

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -5,7 +5,7 @@ export const ProductList = () => {
 
     useEffect(
         () => {
-            fetch("http://localhost:8088/products?_expand=productType")
+            fetch("http://localhost:8088/products?_expand=productType&_sort=productTypeId&_order=asc")
             .then(res => res.json())
             .then((productArray) => {
                 setProducts(productArray)
